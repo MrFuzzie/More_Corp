8eeff583918b81495969fd3c03573c68b64e3a10
J3llyBean <34090593+J3llyBean@users.noreply.github.com>
2019-11-16 11:41:57 +0200

    Update Nav

---
+++ resources/views/layouts/visitor-nav.blade.php
@@ -0,0 +1,49 @@
+
+<nav class="navbar navbar-expand-md sticky-top  navbar-dark bg-dark navbar-laravel flex-md-nowrap shadow">
+
+    <a class="navbar-brand bg-transparent shadow-none col-sm-3 col-md-2 mr-0" href="/">{{ config('app.name', 'Laravel') }}</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item mr-2 border-bottom border-light">
+                <a href="#" class="nav-link text-white">Home</a>
+            </li>
+            <li class="nav-item mr-2 border-bottom border-light">
+                <a href="#" class="nav-link text-white">About</a>
+            </li>
+            <li class="nav-item mr-2 border-bottom border-light">
+                <a href="#" class="nav-link text-white text-decoration-none">Products</a>
+            </li>
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav px-3">
+            <!-- Authentication Links -->
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Sign in') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+            @else
+                <li class="nav-item text-nowrap">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    <a class="nav-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Sign out') }}</a>
+                </li>
+            @endguest
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/layouts/visitor.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'More Corp') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+
+<div id="app">
+    @include('layouts.visitor-nav')
+    <main class="mt-4 w-75 mx-auto">
+    @yield('content')
+    </main>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.6/holder.js"></script>
+@yield('js')
+
+</body>
+</html>
